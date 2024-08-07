6e786b55721b856665c8d84985f4a29c5db19e0a
MeetMavani <meetmavani14@gmail.com>
2024-08-07 20:39:45 +0530

    added the optimal soln for array containing 0's and positives only

--- DSA/Arrays/Easy/LongestSubArrayWithSumK.java
+++ DSA/Arrays/Easy/LongestSubArrayWithSumK.java
@@ -1,4 +1,9 @@
-// Brute-Force approach: TC of O(n*n) and SC of O(1)
+// Brute-Force approach: (The positive function) it takes TC of O(n*n) and SC of O(1)
+// Optimal: It is the optimal approach only if the array contains 0's and positives,
+//          It takes TC of O(2n), why? you must be telling there are two while loops, 
+//          so the reason for 2n is that the second while loop never iterates to n times for every iteration of outer while loop 
+//          instead it maximum iterates to n times so, TC is calculated as O(n)for outer while loop + O(n) for inner while loop = O(2n)
+// Negative: If the array contains negatives too then negative func is the optimal one
 
 public class LongestSubArrayWithSumK {
 
@@ -21,11 +26,37 @@ public class LongestSubArrayWithSumK {
         return max;
     }
 
+    public static int optimal(int[] nums, int k) {
+        int left = 0; int right = 0;
+        int maxlen = 0;
+        long sum = nums[0]; 
+        int n = nums.length;
+
+        while (right < n) {
+            while (sum > k && left <= right) {
+                sum = sum - nums[left];
+                left++;
+            }
+
+            if (sum == k) {
+                maxlen = Math.max(maxlen, right - left + 1);
+            }
+            
+            right++;
+            if (right < n) {
+                sum += nums[right]; 
+            }
+        }
+
+        return maxlen;
+    }
+
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 1, 1, 1, 1, 4, 2, 3};
-        int k = 3;
-        int ans = positive(nums, k);
-        System.out.println("The size of longest sub-array with sum k is: " + ans);
+        int[] nums = {1, 2, 3, 1, 1, 1, 1, 4, 2, 3};  //10
+        int k = 4;
+        // int ans = positive(nums, k);
+        // System.out.println("The size of longest sub-array with sum k is: " + ans);
+        System.out.println(optimal(nums, k));
     }
 
 }
