aa90c51c2c9f317e4135f4518c2104a25f781cf3
MeetMavani <meetmavani14@gmail.com>
2025-01-05 23:36:27 +0530

    Reverse a string

--- DSA/Strings/ReverseString.java
+++ DSA/Strings/ReverseString.java
@@ -5,27 +5,47 @@
 
 
 public class ReverseString {
-    public static void main(String[] args) {
-        
-        
-        String str = "Kaifu Clan";
+
+
+    public static String Better(String str) {
+
         StringBuilder sb = new StringBuilder(str);
 
         int n = str.length();
-
         int i = 0;
         int j = n-1;
 
 
         while(i < j) {
-    
             sb.setCharAt(i, str.charAt(j));
             sb.setCharAt(j, str.charAt(i));
-            
             i++;
             j--;
         } 
-        System.out.println(sb);   
 
+        return sb.toString();
+    }
+
+    public static String Optimal(char[] str) {
+
+        int i = 0;
+        int j = str.length - 1;
+        
+        while(i < j) {
+            char temp = str[i];
+            str[i] = str[j];
+            str[j] = temp;
+            i++;
+            j--;
+        }
+
+
+        return new String(str);
+    }
+
+    public static void main(String[] args) {
+        
+        String str = "Kaifu Clan";
+        System.out.println("Better: " + Better(str));
     }
 }
