024fa40b6a92770b164da7544ba5827a3b9e6927
MeetMavani <meetmavani14@gmail.com>
2025-01-05 23:36:50 +0530

    Reverse a string

--- DSA/Strings/ReverseString.java
+++ DSA/Strings/ReverseString.java
@@ -1,9 +1,20 @@
-// Approach: We use the two pointer approach for swaping the first-last chars then second-secondlast and so on.
-//           We use String Builder for modifying the string which helps eliminating the use of extra space
-//           like another string(concatenation), stack, char array.
-//           TC O(n) and SC O(1)
+// BruteForce: Stack(EXTRA SPACE)
 
+// Algo: We use the two pointer approach for swaping the first-last chars then second-secondlast and so on.
 
+// StringBuilder(Better): We use String Builder for modifying the string. This still takes extra space.
+//                        TC: O(n) and SC O(n)
+//When you initialize StringBuilder, it internally holds a new buffer to store the string. 
+//This buffer is allocated as extra space, which is not part of the original input string.
+
+// Char Array(Optimal): We which helps eliminating the use of extra space like another string(concatenation), 
+//                      stack, char array.
+//                       TC O(n) and SC O(1)
+//When you use a char[] array, you're working directly with the characters from the input string and modifying them in place.
+//The char[] array is not "extra" in the sense of creating a separate new copy of the data (like StringBuilder does). 
+//It’s just a mutable container that allows changes without needing a new copy of the string.
+
+// Built-in: str.reverse()
 public class ReverseString {
 
 
