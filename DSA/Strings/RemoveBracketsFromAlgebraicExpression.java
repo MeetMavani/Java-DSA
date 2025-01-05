8886eb4c3fa93fa636e597583ee40bb7b6f78b42
MeetMavani <meetmavani14@gmail.com>
2025-01-05 22:51:44 +0530

    Remove Brackets from string

---
+++ DSA/Strings/RemoveBracketsFromAlgebraicExpression.java
@@ -0,0 +1,75 @@
+// BruteForce(SubString): We use the same algorithm created in the problem RemoveVowels and just change the conditon 
+//            for detecting the whitespaces.
+// TC : O(n) due to substring and concatenation and SC: O(n) due to new strings
+// 
+// Better(StringBuilder): another optimal solution that combines the simplicity of using StringBuilder (to avoid 
+//                the inefficiencies of repeatedly creating new String objects) and
+//                the performance of a single-pass iteration.
+// TC : O(n)  for a single pass through the string. SC: O(n) for storing the result in a StringBuilder.
+//
+// Optimal(CharArray): -A char[] array is mutable, so we can overwrite elements in the array as we iterate through it.
+//            -We use a two-pointer technique where one pointer (i) reads through the array, and 
+//             another pointer (count) keeps track of the position where the next valid character should go. 
+//            -This method effectively "shifts" non-bracket characters to the front of the array, and
+//             we return the length of the resulting string.
+//            -Then finally we convert the valid portion of the array back to a string
+//  TC: O(n) as each character is processed once. SC: O(1) in-place modifications minimizing additional space usage.
+
+
+
+public class RemoveBracketsFromAlgebraicExpression {
+
+    public static String Brute(String str) {
+    
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (ch == '(' || ch == ')') {
+                str = str.substring(0, i) + str.substring(i + 1);
+                i--;
+            }
+        }
+        return str;
+    }
+    
+    public static String Better(String str) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < str.length(); i++) {
+            if (str.charAt(i) != '(' && str.charAt(i) != ')') {
+                sb.append(str.charAt(i));
+            }
+        }
+        
+        return sb.toString();
+    }
+    
+    public static int Optimal(char[] str) {
+        int count = 0;
+      
+        for (int i = 0; i < str.length; i++) {
+            if (str[i] != '(' && str[i] != ')') {
+                str[count] = str[i];
+                count++;
+            }
+        }
+        return count; // Length of the valid portion
+    }
+    
+
+    public static void main(String[] args) {
+
+        String str = "a+((b-c)+d)";
+        
+        System.out.println("Brute Approach: " + Brute(str));
+        System.out.println("Better Approach: " + Better(str));
+
+        char[] charr = str.toCharArray();
+        int newLength = Optimal(charr);
+        String result = new String(charr, 0, newLength);
+
+        System.out.println(result);
+
+    }
+}
+
