134640ececb8044284797aacbceebde134838a36
MeetMavani <meetmavani14@gmail.com>
2025-01-29 12:59:42 +0530

    Updated the solution

--- DSA/Arrays/Easy/RemoveDuplicates.java
+++ DSA/Arrays/Easy/RemoveDuplicates.java
@@ -1,18 +1,45 @@
+import java.util.*;
+
 public class RemoveDuplicates {
 
-    public static void main(String[] args) {
-        int[] arr = {1, 1, 2, 2, 2, 3, 3, 4, 4};
-        checkDuplicate(arr);
+
+    public static int remove(int[] arr) {
+        int j = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[j]) {
+                j++;
+                arr[j] = arr[i];
+            }            
+        }
+
+        return j + 1;
     }
 
-    public static void checkDuplicate(int[] arr) {
-        int i = 0;
-        for (int j = 1; j < arr.length; j++) {
-            if (arr[j] != arr[i]) {
-                arr[i+1] = arr[j];
-                i++;
-            }
+    public static int hash(int[] arr) {
+
+        HashSet<Integer> hashSet = new HashSet<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            hashSet.add(arr[i]);
         }
-        System.out.println(i+1);
+
+        int k = hashSet.size();
+        int j = 0;
+        for(int x : hashSet) {
+            arr[j++] = x;
+        }
+        
+        return k;
     }
-}   
+
+    public static void main(String[] args) {
+        
+        int[] arr = {1,1,2,2,2,3,3};
+
+        // System.out.println(remove(arr));
+        // System.out.println(Arrays.toString(arr));
+
+        System.out.println(hash(arr));
+
+    }
+}
